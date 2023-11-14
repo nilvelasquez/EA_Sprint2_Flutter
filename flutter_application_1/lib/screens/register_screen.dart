01500e478791a08105a672d57422e47daa658aed
Bernat Jara <bernat.jara@estudiantat.upc.edu>
2023-11-14 11:24:01 +0100

    Register OK

--- flutter_application_1/lib/screens/register_screen.dart
+++ flutter_application_1/lib/screens/register_screen.dart
@@ -34,7 +34,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       );
 
       // Verifica si la solicitud fue exitosa (código 200)
-      if (response.statusCode == 200) {
+      if (response.statusCode == 201) {
         // Registro exitoso, redirige a la pantalla de inicio de sesión
         // ignore: use_build_context_synchronously
         Navigator.pushReplacement(
